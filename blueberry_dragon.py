f648c8c694868ab859e8edd1b1e386c6ad210e30
Taylor Perkins <taylorperkins.dev@gmail.com>
2019-05-09 20:19:34 -0500

    Started converting it to a game class.

--- blueberry_dragon.py
+++ blueberry_dragon.py
@@ -31,7 +31,7 @@ INVULNTIME = 2                            # how long the player is invulnerable 
 GAMEOVERTIME = 4                          # how long the "game over" text stays on the screen in seconds
 MAXHEALTH = 3                             # how much health the player starts with
 
-NUMGRASS = 80                             # number of grass objects in the active area
+NUMROCKS = 80                             # number of grass objects in the active area
 NUMSQUIRRELS = 30                         # number of squirrels in the active area
 SQUIRRELMINSPEED = 3                      # slowest squirrel speed
 SQUIRRELMAXSPEED = 7                      # fastest squirrel speed
@@ -69,25 +69,327 @@ Grass data structure keys:
 """
 
 
+class BDGame:
+    """SCREEN ATTRIBUTES"""
+    FPS = 30  # frames per second to update the screen
+    WINWIDTH = 640  # width of the program's window, in pixels
+    WINHEIGHT = 480  # height in pixels
+    HALF_WINWIDTH = int(WINWIDTH / 2)
+    HALF_WINHEIGHT = int(WINHEIGHT / 2)
+
+    """COLORS"""
+    GRASSCOLOR = (163, 139, 115)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+
+    """"""
+    CAMERASLACK = 90  # how far from the center the squirrel moves before moving the camera
+    MOVERATE = 9  # how fast the player moves
+    BOUNCERATE = 6  # how fast the player bounces (large is slower)
+    BOUNCEHEIGHT = 30  # how high the player bounces
+    STARTSIZE = 25  # how big the player starts off
+    WINSIZE = 300  # how big the player needs to be to win
+    INVULNTIME = 2  # how long the player is invulnerable after being hit in seconds
+    GAMEOVERTIME = 4  # how long the "game over" text stays on the screen in seconds
+    MAXHEALTH = 3  # how much health the player starts with
+
+    NUMROCKS = 80  # number of rock objects in the active area
+    NUMSQUIRRELS = 30  # number of squirrels in the active area
+    SQUIRRELMINSPEED = 3  # slowest squirrel speed
+    SQUIRRELMAXSPEED = 7  # fastest squirrel speed
+    DIRCHANGEFREQ = 2  # % chance of direction change per frame
+    LEFT = 'left'
+    RIGHT = 'right'
+
+    def __init__(self):
+        # set up variables for the start of a new game
+        self.invulnerable_mode = False  # if the player is invulnerable
+        self.invulnerable_start_time = 0  # time the player became invulnerable
+        self.game_over_mode = False  # if the player has lost
+        self.game_over_start_time = 0  # time the player lost
+        self.win_mode = False  # if the player has won
+
+        # create the surfaces to hold game text
+        self.game_over_surf = BASICFONT.render('Game Over', True, WHITE)
+        self.game_over_rect = self.game_over_surf.get_rect()
+        self.game_over_rect.center = (HALF_WINWIDTH, HALF_WINHEIGHT)
+
+        self.win_surf = BASICFONT.render('You have achieved OMEGA DRAGON!', True, WHITE)
+        self.win_rect = self.win_surf.get_rect()
+        self.win_rect.center = (HALF_WINWIDTH, HALF_WINHEIGHT)
+
+        self.win_surf2 = BASICFONT.render('(Press "r" to restart.)', True, WHITE)
+        self.win_rect2 = self.win_surf2.get_rect()
+        self.win_rect2.center = (HALF_WINWIDTH, HALF_WINHEIGHT + 30)
+
+        # camera_x and camera_y are the top left of where the camera view is
+        self.camera_x = 0
+        self.camera_y = 0
+
+        # stores the player object:
+        self.player_obj = {
+            'surface': pygame.transform.scale(L_DRAGON_IMG, (STARTSIZE, STARTSIZE)),
+            'facing': LEFT,
+            'size': STARTSIZE,
+            'x': HALF_WINWIDTH,
+            'y': HALF_WINHEIGHT,
+            'bounce': 0,
+            'health': MAXHEALTH}
+
+        self.move_left = False
+        self.move_right = False
+        self.move_up = False
+        self.move_down = False
+
+        self.rock_objs = []  # stores all the grass objects in the game
+        self.dragon_objs = []  # stores all the non-player squirrel objects
+
+        # start off with some random grass images on the screen
+        for i in range(10):
+            self.rock_objs.append(make_new_grass(self.camera_x, self.camera_y))
+            self.rock_objs[i]['x'] = random.randint(0, WINWIDTH)
+            self.rock_objs[i]['y'] = random.randint(0, WINHEIGHT)
+
+    def _check_invulnerable_mode(self):
+        # Check if we should turn off invulnerability
+        if self.invulnerable_mode and time.time() - self.invulnerable_start_time > INVULNTIME:
+            self.invulnerable_mode = False
+
+    def _move_dragon_objs(self):
+        for d_obj in self.dragon_objs:
+            # move the dragon, and adjust for their bounce
+            d_obj['x'] += d_obj['movex']
+            d_obj['y'] += d_obj['movey']
+            d_obj['bounce'] += 1
+
+            if d_obj['bounce'] > d_obj['bouncerate']:
+                # reset bounce amount
+                d_obj['bounce'] = 0
+
+            # random chance they change direction
+            if random.randint(0, 99) < DIRCHANGEFREQ:
+                d_obj['movex'] = get_random_velocity()
+                d_obj['movey'] = get_random_velocity()
+
+                # faces right
+                if d_obj['movex'] > 0:
+                    d_obj['surface'] = pygame.transform.scale(R_DRAGON_IMG, (d_obj['width'], d_obj['height']))
+                # faces left
+                else:
+                    d_obj['surface'] = pygame.transform.scale(L_DRAGON_IMG, (d_obj['width'], d_obj['height']))
+
+    def _delete_unused_objs(self, obj_list):
+        # go through all the objects and see if any need to be deleted.
+        for i in range(len(obj_list) - 1, -1, -1):
+            if is_outside_active_area(self.camera_x, self.camera_y, obj_list[i]):
+                del obj_list[i]
+
+    def run_game(self):
+
+        # main game loop
+        while True:
+            self._check_invulnerable_mode()
+            self._move_dragon_objs()
+
+            self._delete_unused_objs(self.rock_objs)
+            self._delete_unused_objs(self.dragon_objs)
+
+            # add more grass & squirrels if we don't have enough.
+            while len(rock_objs) < NUMROCKS:
+                rock_objs.append(make_new_grass(camera_x, camera_y))
+            while len(dragon_objs) < NUMSQUIRRELS:
+                dragon_objs.append(make_new_dragon(camera_x, camera_y))
+
+            # adjust camera_x and camera_y if beyond the "camera slack"
+            player_center_x = player_obj['x'] + int(player_obj['size'] / 2)
+            player_center_y = player_obj['y'] + int(player_obj['size'] / 2)
+
+            if (camera_x + HALF_WINWIDTH) - player_center_x > CAMERASLACK:
+                camera_x = player_center_x + CAMERASLACK - HALF_WINWIDTH
+            elif player_center_x - (camera_x + HALF_WINWIDTH) > CAMERASLACK:
+                camera_x = player_center_x - CAMERASLACK - HALF_WINWIDTH
+            if (camera_y + HALF_WINHEIGHT) - player_center_y > CAMERASLACK:
+                camera_y = player_center_y + CAMERASLACK - HALF_WINHEIGHT
+            elif player_center_y - (camera_y + HALF_WINHEIGHT) > CAMERASLACK:
+                camera_y = player_center_y - CAMERASLACK - HALF_WINHEIGHT
+
+            # draw the green background
+            DISPLAYSURF.fill(GRASSCOLOR)
+
+            # draw all the rock objects on the screen
+            for r_obj in rock_objs:
+                r_rect = pygame.Rect((r_obj['x'] - camera_x,
+                                      r_obj['y'] - camera_y,
+                                      r_obj['width'],
+                                      r_obj['height']))
+                DISPLAYSURF.blit(GRASSIMAGES[r_obj['grassImage']], r_rect)
+
+            # draw the other dragons
+            for d_obj in dragon_objs:
+                d_obj['rect'] = pygame.Rect((d_obj['x'] - camera_x,
+                                             d_obj['y'] - camera_y - get_bounce_amount(d_obj['bounce'],
+                                                                                       d_obj['bouncerate'],
+                                                                                       d_obj['bounceheight']),
+                                             d_obj['width'],
+                                             d_obj['height']))
+                DISPLAYSURF.blit(d_obj['surface'], d_obj['rect'])
+
+            # draw the player dragon
+            flash_is_on = round(time.time(), 1) * 10 % 2 == 1
+            if not game_over_mode and not (invulnerable_mode and flash_is_on):
+                player_obj['rect'] = pygame.Rect((player_obj['x'] - camera_x,
+                                                  player_obj['y'] - camera_y - get_bounce_amount(player_obj['bounce'],
+                                                                                                 BOUNCERATE,
+                                                                                                 BOUNCEHEIGHT),
+                                                  player_obj['size'],
+                                                  player_obj['size']))
+                DISPLAYSURF.blit(player_obj['surface'], player_obj['rect'])
+
+            # draw the health meter
+            draw_health_meter(player_obj['health'])
+
+            # event handling loop
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    terminate()
+
+                elif event.type == KEYDOWN:
+                    if event.key in (K_UP, K_w):
+                        move_down = False
+                        move_up = True
+
+                    elif event.key in (K_DOWN, K_s):
+                        move_up = False
+                        move_down = True
+
+                    elif event.key in (K_LEFT, K_a):
+                        move_right = False
+                        move_left = True
+
+                        # change player image
+                        if player_obj['facing'] != LEFT:
+                            player_obj['surface'] = pygame.transform.scale(L_DRAGON_IMG,
+                                                                           (player_obj['size'], player_obj['size']))
+
+                        player_obj['facing'] = LEFT
+
+                    elif event.key in (K_RIGHT, K_d):
+                        move_left = False
+                        move_right = True
+
+                        # change player image
+                        if player_obj['facing'] != RIGHT:
+                            player_obj['surface'] = pygame.transform.scale(R_DRAGON_IMG,
+                                                                           (player_obj['size'], player_obj['size']))
+
+                        player_obj['facing'] = RIGHT
+
+                    elif win_mode and event.key == K_r:
+                        return
+
+                elif event.type == KEYUP:
+                    # stop moving the player's squirrel
+                    if event.key in (K_LEFT, K_a):
+                        move_left = False
+                    elif event.key in (K_RIGHT, K_d):
+                        move_right = False
+                    elif event.key in (K_UP, K_w):
+                        move_up = False
+                    elif event.key in (K_DOWN, K_s):
+                        move_down = False
+
+                    elif event.key == K_ESCAPE:
+                        terminate()
+
+            if not game_over_mode:
+                # actually move the player
+                if move_left:
+                    player_obj['x'] -= MOVERATE
+                if move_right:
+                    player_obj['x'] += MOVERATE
+                if move_up:
+                    player_obj['y'] -= MOVERATE
+                if move_down:
+                    player_obj['y'] += MOVERATE
+
+                if (move_left or move_right or move_up or move_down) or player_obj['bounce'] != 0:
+                    player_obj['bounce'] += 1
+
+                if player_obj['bounce'] > BOUNCERATE:
+                    # reset bounce amount
+                    player_obj['bounce'] = 0
+
+                # check if the player has collided with any dragons
+                for i in range(len(dragon_objs) - 1, -1, -1):
+                    dragon_obj = dragon_objs[i]
+                    if 'rect' in dragon_obj and player_obj['rect'].colliderect(dragon_obj['rect']):
+
+                        # a player/dragon collision has occurred
+                        if dragon_obj['width'] * dragon_obj['height'] <= player_obj['size'] ** 2:
+                            # player is larger and eats the squirrel
+                            player_obj['size'] += int((dragon_obj['width'] * dragon_obj['height']) ** 0.2) + 1
+                            del dragon_objs[i]
+
+                            if player_obj['facing'] == LEFT:
+                                player_obj['surface'] = pygame.transform.scale(L_DRAGON_IMG,
+                                                                               (player_obj['size'], player_obj['size']))
+                            if player_obj['facing'] == RIGHT:
+                                player_obj['surface'] = pygame.transform.scale(R_DRAGON_IMG,
+                                                                               (player_obj['size'], player_obj['size']))
+
+                            if player_obj['size'] > WINSIZE:
+                                # turn on "win mode"
+                                win_mode = True
+
+                        elif not invulnerable_mode:
+                            # player is smaller and takes damage
+                            invulnerable_mode = True
+                            invulnerable_start_time = time.time()
+                            player_obj['health'] -= 1
+                            if player_obj['health'] == 0:
+                                game_over_mode = True  # turn on "game over mode"
+                                game_over_start_time = time.time()
+
+            else:
+                # game is over, show "game over" text
+                DISPLAYSURF.blit(game_over_surf, game_over_rect)
+                if time.time() - game_over_start_time > GAMEOVERTIME:
+                    # end the current game
+                    return
+
+            # check if the player has won
+            if win_mode:
+                DISPLAYSURF.blit(win_surf, win_rect)
+                DISPLAYSURF.blit(win_surf2, win_rect2)
+
+            pygame.display.update()
+            FPSCLOCK.tick(FPS)
+
+
 def main():
     global FPSCLOCK, DISPLAYSURF, BASICFONT, L_DRAGON_IMG, R_DRAGON_IMG, GRASSIMAGES
 
     pygame.init()
+
     FPSCLOCK = pygame.time.Clock()
-    pygame.display.set_icon(pygame.image.load('./images/gameicon.png'))
     DISPLAYSURF = pygame.display.set_mode((WINWIDTH, WINHEIGHT))
-    pygame.display.set_caption('Blueberry Dragon Eat Blueberry Dragon')
     BASICFONT = pygame.font.Font('freesansbold.ttf', 32)
 
+    pygame.display.set_icon(pygame.image.load('./images/gameicon.png'))
+    pygame.display.set_caption('Blueberry Dragon Eat Blueberry Dragon')
+
     # load the image files
-    R_DRAGON_IMG = pygame.image.load('./images/blueberry-dragon.png')
     L_DRAGON_IMG = pygame.transform.flip(R_DRAGON_IMG, True, False)
+    R_DRAGON_IMG = pygame.image.load('./images/blueberry-dragon.png')
+
     GRASSIMAGES = []
     for i in range(1, 4):
         GRASSIMAGES.append(pygame.image.load('./images/rock%s.png' % i))
 
+    game = BDGame()
+
     while True:
-        run_game()
+        game.run_game()
 
 
 def run_game():
@@ -115,7 +417,7 @@ def run_game():
     camera_x = 0
     camera_y = 0
 
-    grass_objs = []    # stores all the grass objects in the game
+    rock_objs = []    # stores all the grass objects in the game
     dragon_objs = []   # stores all the non-player squirrel objects
     # stores the player object:
     player_obj = {
@@ -134,9 +436,9 @@ def run_game():
 
     # start off with some random grass images on the screen
     for i in range(10):
-        grass_objs.append(make_new_grass(camera_x, camera_y))
-        grass_objs[i]['x'] = random.randint(0, WINWIDTH)
-        grass_objs[i]['y'] = random.randint(0, WINHEIGHT)
+        rock_objs.append(make_new_grass(camera_x, camera_y))
+        rock_objs[i]['x'] = random.randint(0, WINWIDTH)
+        rock_objs[i]['y'] = random.randint(0, WINHEIGHT)
 
     # main game loop
     while True:
@@ -167,16 +469,16 @@ def run_game():
                     d_obj['surface'] = pygame.transform.scale(L_DRAGON_IMG, (d_obj['width'], d_obj['height']))
 
         # go through all the objects and see if any need to be deleted.
-        for i in range(len(grass_objs) - 1, -1, -1):
-            if is_outside_active_area(camera_x, camera_y, grass_objs[i]):
-                del grass_objs[i]
+        for i in range(len(rock_objs) - 1, -1, -1):
+            if is_outside_active_area(camera_x, camera_y, rock_objs[i]):
+                del rock_objs[i]
         for i in range(len(dragon_objs) - 1, -1, -1):
             if is_outside_active_area(camera_x, camera_y, dragon_objs[i]):
                 del dragon_objs[i]
 
         # add more grass & squirrels if we don't have enough.
-        while len(grass_objs) < NUMGRASS:
-            grass_objs.append(make_new_grass(camera_x, camera_y))
+        while len(rock_objs) < NUMROCKS:
+            rock_objs.append(make_new_grass(camera_x, camera_y))
         while len(dragon_objs) < NUMSQUIRRELS:
             dragon_objs.append(make_new_dragon(camera_x, camera_y))
 
@@ -197,7 +499,7 @@ def run_game():
         DISPLAYSURF.fill(GRASSCOLOR)
 
         # draw all the rock objects on the screen
-        for r_obj in grass_objs:
+        for r_obj in rock_objs:
             r_rect = pygame.Rect((r_obj['x'] - camera_x,
                                   r_obj['y'] - camera_y,
                                   r_obj['width'],
